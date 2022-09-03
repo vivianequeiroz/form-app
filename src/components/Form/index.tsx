546198556fc9092bdfc8daba61ae790dcff86c5d
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2022-09-03 12:30:55 -0300

    feat: using the controlled input and receiving the data object at submit

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,44 +1,59 @@
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
-import { Button } from '../Button';
-import { Input } from '../Input';
-import { Container } from './styles';
+import React from "react";
+import { Button } from "../Button";
+import { ControlledInput } from "../ControlledInput";
+import { Container } from "./styles";
+import { useForm } from "react-hook-form";
 
-const VALID_EMAIL_EXPRESSION = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  passwor_confirm: string;
+};
+
+const VALID_EMAIL_EXPRESSION =
+  /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
 export function Form() {
+  const { control, handleSubmit } = useForm<FormData>();
 
-  function handleUserRegister() {
-
+  function handleUserRegister(data: FormData) {
+    console.log(data);
+    //it can be send at an api call
   }
 
   return (
     <Container>
-      <Input
+      <ControlledInput
+        control={control}
         icon="user"
+        name="name"
         placeholder="Nome"
       />
-      <Input
+      <ControlledInput
+        name="email"
+        control={control}
         icon="mail"
         placeholder="E-mail"
         keyboardType="email-address"
-        autoCapitalize='none'
+        autoCapitalize="none"
       />
-      <Input
+      <ControlledInput
+        control={control}
+        name="password"
         icon="lock"
         placeholder="Senha"
         secureTextEntry
       />
-      <Input
+      <ControlledInput
+        control={control}
+        name="passwor_confirm"
         icon="lock"
         placeholder="Confirme a senha"
         secureTextEntry
       />
 
-      <Button
-        title="Cadastrar"
-        onPress={handleUserRegister}
-      />
+      <Button title="Cadastrar" onPress={handleSubmit(handleUserRegister)} />
     </Container>
-  )
+  );
 }
