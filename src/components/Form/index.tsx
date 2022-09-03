82323e7927fc1289b58f35bc1f18eb1071846e5e
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2022-09-03 12:48:29 -0300

    feat: add the logic of input validation using yup

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,6 +3,8 @@ import { Button } from "../Button";
 import { ControlledInput } from "../ControlledInput";
 import { Container } from "./styles";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type FormData = {
   name: string;
@@ -11,8 +13,22 @@ type FormData = {
   password_confirm: string;
 };
 
+const schema = yup.object({
+  name: yup.string().required("Informe o seu nome"),
+  email: yup.string().email("E-mail inválido").required("Informe o seu e-mail"),
+  password: yup
+    .string()
+    .min(6, "Mínimo de 6 caracteres")
+    .required("Informe uma senha"),
+  password_confirm: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "As senhas não coincidem"),
+});
+
 export function Form() {
-  const { control, handleSubmit } = useForm<FormData>();
+  const { control, handleSubmit } = useForm<FormData>({
+    resolver: yupResolver(schema), //form will be validaded according to this schema
+  });
 
   function handleUserRegister(data: FormData) {
     console.log(data);
